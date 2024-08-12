b1ea353df096912923269c2335ba1a688596b3dc
Rumon360 <hmk.rumon@gmail.com>
2024-08-12 21:22:01 +0600

    index.js on root

---
+++ index.js
@@ -0,0 +1,9 @@
+import CustomCursor from "./js/custom-cursor.js";
+import FirstPageAnimation from "./js/first-page.js";
+import SmoothScroll from "./js/smooth-scroll.js";
+import TabsAnimation from "./js/tabs-animation.js";
+
+SmoothScroll();
+CustomCursor();
+FirstPageAnimation();
+TabsAnimation();
--- js/index.js
+++
@@ -1,9 +0,0 @@
-import CustomCursor from "./custom-cursor.js";
-import FirstPageAnimation from "./first-page.js";
-import SmoothScroll from "./smooth-scroll.js";
-import TabsAnimation from "./tabs-animation.js";
-
-SmoothScroll();
-CustomCursor();
-FirstPageAnimation();
-TabsAnimation();
